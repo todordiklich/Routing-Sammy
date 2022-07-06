e2b7083b9233ecd54bf27cbf02b1f2da16405b08
Todor Diklich <todordiklich@gmail.com>
2022-07-06 11:31:05 +0300

    bug fix

--- TeamManager-Routing/scripts/app.js
+++ TeamManager-Routing/scripts/app.js
@@ -1,12 +1,12 @@
 const auth = firebase.auth();
 const databaseURL = `https://team-manager-6f161-default-rtdb.firebaseio.com/`;
-const errorBoxElement = document.getElementById("errorBox");
-const infoBoxElement = document.getElementById("infoBox");
+const errorBoxElement = document.getElementById('errorBox');
+const infoBoxElement = document.getElementById('infoBox');
 
 //var database = firebase.database();
 
-const router = Sammy("#main", function () {
-  this.use("Handlebars", "hbs");
+const router = Sammy('#main', function () {
+  this.use('Handlebars', 'hbs');
   // this.use('Handlebars', 'hbs');  // let Sammy know how to use the .hbs extensions files
 
   // all the partials inside hbs => diff 'variables' and i can get each of them
@@ -14,91 +14,117 @@ const router = Sammy("#main", function () {
   // variable the name can be whatever i want )
 
   //GET
-  this.get("/home", function (context) {
+  this.get('/home', function (context) {
     verifyUser(context);
 
     this.loadPartials({
-      header: "./templates/common/header.hbs",
-      footer: "./templates/common/footer.hbs",
+      header: './templates/common/header.hbs',
+      footer: './templates/common/footer.hbs',
     })
       .then(function () {
-        this.partial("./templates/home/home.hbs");
+        this.partial('./templates/home/home.hbs');
       })
       .catch((e) => errorHandler(e));
   });
 
-  this.get("/about", function (context) {
+  this.get('/about', function (context) {
+    verifyUser(context);
     this.loadPartials({
-      header: "./templates/common/header.hbs",
-      footer: "./templates/common/footer.hbs",
+      header: './templates/common/header.hbs',
+      footer: './templates/common/footer.hbs',
     })
       .then(function () {
-        this.partial("./templates/about/about.hbs");
+        this.partial('./templates/about/about.hbs');
       })
       .catch((e) => errorHandler(e));
   });
 
-  this.get("/register", function (context) {
+  this.get('/register', function (context) {
     this.loadPartials({
-      header: "./templates/common/header.hbs",
-      footer: "./templates/common/footer.hbs",
-      registerForm: "./templates/register/registerForm.hbs",
+      header: './templates/common/header.hbs',
+      footer: './templates/common/footer.hbs',
+      registerForm: './templates/register/registerForm.hbs',
     })
       .then(function () {
-        this.partial("./templates/register/registerPage.hbs");
+        this.partial('./templates/register/registerPage.hbs');
       })
       .catch((e) => errorHandler(e));
   });
 
-  this.get("/login", function (context) {
+  this.get('/login', function (context) {
     this.loadPartials({
-      header: "./templates/common/header.hbs",
-      footer: "./templates/common/footer.hbs",
-      loginForm: "./templates/login/loginForm.hbs",
+      header: './templates/common/header.hbs',
+      footer: './templates/common/footer.hbs',
+      loginForm: './templates/login/loginForm.hbs',
     })
       .then(function () {
-        this.partial("./templates/login/loginPage.hbs");
+        this.partial('./templates/login/loginPage.hbs');
+      })
+      .catch((e) => errorHandler(e));
+  });
+
+  this.get('/logout', function (context) {
+    sessionStorage.removeItem('userData');
+
+    this.redirect('/home')
+      .then(function () {
+        showTimeoutMessage(infoBoxElement, 'You have logged out!');
+      })
+      .catch((e) => errorHandler(e));
+  });
+
+  this.get('/catalog', function (context) {
+    verifyUser(context);
+
+    this.loadPartials({
+      header: './templates/common/header.hbs',
+      footer: './templates/common/footer.hbs',
+      team: './templates/catalog/team.hbs',
+    })
+      .then(function () {
+        this.partial('./templates/catalog/teamCatalog.hbs');
       })
       .catch((e) => errorHandler(e));
   });
 
   //POST
-  this.post("/register", function (context) {
+  this.post('/register', function (context) {
     const { username, password, repeatPassword } = context.params;
 
     if (password !== repeatPassword) {
-      showTimeoutMessage(errorBoxElement, "The passwords does not match!");
+      showTimeoutMessage(errorBoxElement, 'The passwords does not match!');
     }
 
     auth
       .createUserWithEmailAndPassword(username, password)
       .then(() => {
-        this.redirect("/login");
+        this.redirect('/login');
       })
       .catch(errorHandler(e));
   });
 
-  this.post("/login", function (context) {
+  this.post('/login', function (context) {
     const { email: username, password } = context.params;
 
     auth
       .signInWithEmailAndPassword(username, password)
       .then((user) => {
-        sessionStorage.setItem("userData", JSON.stringify(user));
+        sessionStorage.setItem('userData', JSON.stringify(user));
         context.loggedIn = true;
-        this.redirect("/home");
+        this.redirect('/home');
       })
       .catch(errorHandler(e));
   });
 });
 
-router.run("/home");
+router.run('/home');
 
 function verifyUser(context) {
-  let user = JSON.parse(sessionStorage.getItem("userData"));
+  let user = JSON.parse(sessionStorage.getItem('userData'));
 
   if (user) {
     context.loggedIn = true;
+    context.hasNoTeam = true;
     context.email = user.user.email;
   }
 }
@@ -109,10 +135,10 @@ function errorHandler(e) {
 
 function showTimeoutMessage(element, message) {
   element.innerText = message;
-  element.style.display = "block";
+  element.style.display = 'block';
 
   setTimeout(() => {
-    element.innerText = "";
-    element.style.display = "none";
+    element.innerText = '';
+    element.style.display = 'none';
   }, 3000);
 }
